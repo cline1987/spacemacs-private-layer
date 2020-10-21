9215888ee55a64e6488c5f2c47e851cc07050b94
Delin Wang <dlwangnju@gmail.com>
2020-10-21 15:23:57 +0800

    modified init.el

--- init.el
+++ init.el
@@ -47,20 +47,20 @@ values."
      ;; common-lisp
      ;; dash
      emacs-lisp
-     extra-langs
+     ;; extra-langs
      ;; git
      ;; html
      ibuffer
      ;; javascript
      latex
-     markdown
+     ;; markdown
      ;; org
      python
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      ;; shell-scripts
-     sql
+     ;; sql
      spell-checking
      syntax-checking
      themes-megapack
